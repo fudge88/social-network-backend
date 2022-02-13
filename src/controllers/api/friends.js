749ad4869b4c65cde94e4c686b4bd91be09863f9
Fahra Akhlaq <f.akhlaq@outlook.com>
2022-02-13 00:38:34 +0000

    unable to create reactions in controllers

--- src/controllers/api/friends.js
+++ src/controllers/api/friends.js
@@ -1,6 +1,24 @@
-const createFriendForUser = (req, res) => {
-  res.send("createFriendForUser");
+const { User } = require("../../models");
+
+const createFriendForUser = async (req, res) => {
+  try {
+    const { userName, email } = req.body;
+    if (userName && email) {
+      const newUser = await User.create({ userName, email });
+      return res.json({ success: true, data: newUser });
+    }
+    return res.status(400).json({
+      success: false,
+      error: "Please complete the username and email",
+    });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create new user | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to create new user" });
+  }
 };
+
 const deleteFriendByUser = (req, res) => {
   const { userId, friendId } = req.params;
   console.log("userId", userId);
--- src/controllers/api/reactions.js
+++ src/controllers/api/reactions.js
@@ -1,6 +1,47 @@
-const createReactionForThought = (req, res) => {
-  res.send("createReactionForThought");
+const { Thought, schema } = require("../../models");
+
+const createReactionForThought = async (req, res) => {
+  // try {
+  //   const { reactionBody, userName } = req.params;
+  //   const newReaction = await Thought.reactions.create({
+  //     reactionBody,
+  //     userName,
+  //   });
+  //   return res.json({ success: true, data: newReaction });
+  // } catch (error) {
+  //   console.log(`[ERROR]: Failed to create reaction | ${error.message}`);
+  //   return res
+  //     .status(500)
+  //     .json({ success: false, error: "Failed to create reaction" });
+  // }
+  try {
+    const { reactionBody, userName } = req.body;
+    if (reactionBody && userName) {
+      const newReaction = await schema.create({ userName, thoughtText });
+      Thought.reactions.push(newReaction);
+      return res.json({ success: true, data: newThought });
+    }
+    return res.status(400).json({
+      success: false,
+      error: "Please complete the required fields",
+    });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create new thought | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to create new thought" });
+  }
+  // create a comment
+  // const { reactionBody, userName } = req.params;
+  // Thought.reactions.push(reactionBody, userName);
+  // const newReaction = Thought.reactions[0];
+
+  // Thought.save(function (err) {
+  //   if (err) return handleError(err);
+  //   console.log("Success!");
+  // });
 };
+
 const deleteReactionByThought = (req, res) => {
   const { thoughtId, reactionId } = req.params;
   console.log("thoughtId", thoughtId);
